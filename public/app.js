acdd7ee5118b31973ff773acac93a4373ec74ea7
GlitchandGo <ritter.oxley1@gmail.com>
2025-11-26 14:24:15 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-
+window.APP = {
+  storeAuth(data) {
+    localStorage.setItem('astrona_token', data.token);
+    localStorage.setItem('astrona_profile', JSON.stringify(data.profile));
+  },
+  token() { return localStorage.getItem('astrona_token') || ''; },
+  me() { try { return JSON.parse(localStorage.getItem('astrona_profile') || '{}'); } catch { return {}; } },
+  ensureAuth() {
+    if (!this.token()) location.href = 'index.html';
+  },
+  updateProfile(profile) {
+    localStorage.setItem('astrona_profile', JSON.stringify(profile));
+  }
+};
