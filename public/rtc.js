c9d729945819b81de5cb23a8ae23a6f1130e158a
GlitchandGo <ritter.oxley1@gmail.com>
2025-11-26 14:26:32 -0600

    Update rtc.js

--- public/rtc.js
+++ public/rtc.js
@@ -1,1 +1,8 @@
-
+window.RTC = {
+  createPeer({ audio = true, video = false } = {}) {
+    const pc = new RTCPeerConnection({
+      iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }]
+    });
+    return pc;
+  }
+};
