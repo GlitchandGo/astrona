df4feffcda373c39e351296fed6c31f49e4ac93a
GlitchandGo <ritter.oxley1@gmail.com>
2025-11-26 14:24:49 -0600

    Update ws.js

--- public/ws.js
+++ public/ws.js
@@ -1,1 +1,26 @@
-
+window.WS = (() => {
+  let sock = null;
+  let handlers = [];
+  function connect(onOpen, onMessage) {
+    if (sock && sock.readyState === 1) return;
+    const token = APP.token();
+    sock = new WebSocket(`${location.origin.replace('http', 'ws')}/?token=${token}`);
+    sock.onopen = () => { onOpen && onOpen(); };
+    sock.onmessage = (ev) => {
+      const msg = JSON.parse(ev.data);
+      onMessage && onMessage(msg);
+      for (const h of handlers) h(msg);
+    };
+    sock.onclose = () => {};
+  }
+  function send(type, payload) {
+    if (sock && sock.readyState === 1) {
+      sock.send(JSON.stringify({ type, payload }));
+    }
+  }
+  function delivered(threadId, messageId) {
+    send('deliver', { threadId, messageId });
+  }
+  function on(handler) { handlers.push(handler); }
+  return { connect, send, delivered, on };
+})();
