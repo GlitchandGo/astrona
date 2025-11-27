d7d8d30e0e5a0f672677205dbceebd22b72ca1dd
GlitchandGo <ritter.oxley1@gmail.com>
2025-11-27 11:19:31 -0600

    Update group.js

--- server/models/group.js
+++ server/models/group.js
@@ -1,1 +1,30 @@
+import mongoose from 'mongoose';
 
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    default: 'Group'
+  },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  memberIds: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Optional: add indexes for faster lookups
+groupSchema.index({ ownerId: 1 });
+groupSchema.index({ memberIds: 1 });
+
+const Group = mongoose.model('Group', groupSchema);
+
+export default Group;
