c86db17f7f41c9f9979e5d4abec023b4dc5d574d
GlitchandGo <ritter.oxley1@gmail.com>
2025-11-27 10:33:32 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://glitchandgo:YOUR_PASSWORD@astrona.to8htqh.mongodb.net/Astrona";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://glitchandgo:Ls898dia@astrona.to8htqh.mongodb.net/Astrona";
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
